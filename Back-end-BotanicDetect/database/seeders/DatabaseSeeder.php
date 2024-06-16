8c1e6f4446d7c4045254b18378a46022983d86ac
mufidmoesz <93044827+mufidmoesz@users.noreply.github.com>
2024-06-16 19:56:15 +0700

    add treatment seeder

--- Back-end-BotanicDetect/database/seeders/DatabaseSeeder.php
+++ Back-end-BotanicDetect/database/seeders/DatabaseSeeder.php
@@ -5,29 +5,34 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use App\Models\PlantDisease;
 use App\Models\Plant;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $diseases = [
-            ['name' => 'Tomato Leaf Mold', 'treatment' => 'Remove infected leaves, apply fungicide.'],
-            ['name' => 'Tomato healthy', 'treatment' => 'No treatment required.'],
-            ['name' => 'Apple Cedar apple rust', 'treatment' => 'Prune affected branches, apply fungicide.'],
-            ['name' => 'Apple healthy', 'treatment' => 'No treatment required.'],
-            ['name' => 'Grape healthy', 'treatment' => 'No treatment required.'],
-            ['name' => 'Grape Black rot', 'treatment' => 'Remove infected grapes, apply fungicide.'],
-            ['name' => 'Strawberry healthy', 'treatment' => 'No treatment required.'],
-            ['name' => 'Strawberry Leaf scorch', 'treatment' => 'Remove infected leaves, apply fungicide.'],
-            ['name' => 'Potato healthy', 'treatment' => 'No treatment required.'],
-            ['name' => 'Potato Late blight', 'treatment' => 'Remove infected leaves, apply fungicide.'],
-        ];
+        // $diseases = [
+        //     ['name' => 'Tomato Leaf Mold', 'treatment' => 'Remove infected leaves, apply fungicide.'],
+        //     ['name' => 'Tomato healthy', 'treatment' => 'No treatment required.'],
+        //     ['name' => 'Apple Cedar apple rust', 'treatment' => 'Prune affected branches, apply fungicide.'],
+        //     ['name' => 'Apple healthy', 'treatment' => 'No treatment required.'],
+        //     ['name' => 'Grape healthy', 'treatment' => 'No treatment required.'],
+        //     ['name' => 'Grape Black rot', 'treatment' => 'Remove infected grapes, apply fungicide.'],
+        //     ['name' => 'Strawberry healthy', 'treatment' => 'No treatment required.'],
+        //     ['name' => 'Strawberry Leaf scorch', 'treatment' => 'Remove infected leaves, apply fungicide.'],
+        //     ['name' => 'Potato healthy', 'treatment' => 'No treatment required.'],
+        //     ['name' => 'Potato Late blight', 'treatment' => 'Remove infected leaves, apply fungicide.'],
+        // ];
 
-        foreach ($diseases as $disease) {
+        // read cure.json and make seeder based on key as name and value as treatment
+        $file = File::get(database_path('seeders/cure.json'));
+        $diseases = json_decode($file, true);
+
+        foreach ($diseases as $name => $treatment) {
             PlantDisease::create([
-                'name' => $disease['name'],
-                'treatment' => $disease['treatment'],
-                'slug' => Str::slug($disease['name']),
+                'name' => $name,
+                'treatment' => $treatment,
+                'slug' => Str::slug($name),
             ]);
         }
     }
